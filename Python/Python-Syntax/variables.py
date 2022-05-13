2944efcc98b14b1e1362a0c21ad9bb1d1eb39beb
Advay Vyas <advay.vyas@gmail.com>
2022-05-12 22:48:54 -0500

    Minor commit: casting + conditional

--- Python/Python-Syntax/variables.py
+++ Python/Python-Syntax/variables.py
@@ -3,3 +3,14 @@ print(num)
 
 string = "hello world"
 print(string)
+
+if 5 > 2:
+    print("Five is greater than two!")
+# example from w3schools.com
+
+# Casting
+x = 5
+x = str(x)
+print(x)
+
+# 
