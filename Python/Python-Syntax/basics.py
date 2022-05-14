1183b8e2b10885fb5d96b60fb8e81b070dc569f1
Advay Vyas <advay.vyas@gmail.com>
2022-05-14 10:16:00 -0500

    Variable types list

--- Python/Python-Syntax/basics.py
+++ Python/Python-Syntax/basics.py
@@ -44,4 +44,24 @@ def variableTest():
 
 variableTest()
 
+# Types
+x = "Hello World" #str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True #bool	
+x = b"Hello" #bytes	
+x = bytearray(5) #bytearray	
+x = memoryview(bytes(5)) #memoryview	
+x = None #NoneType	
+
+# You can also set specific data types
+x = str("Hello World")
+x = int(2)
 
