aa4cfdb6a688967c321da7b32fd6736fafc810d3
Advay Vyas <advay.vyas@gmail.com>
2022-05-13 19:02:18 -0500

    Multiple variable assignment and one value multiple variable assignment

--- Python/Python-Syntax/variables.py
+++ Python/Python-Syntax/variables.py
@@ -13,4 +13,11 @@ x = 5
 x = str(x)
 print(x)
 
-# 
+# single or double quotes are the same in Python
+
+# dashes, spaces, and numbers are illegal variable names
+
+x, y, z = "Orange", "Banana", "Cherry"
+x = y = z = "Orange"
+# examples from w3schools.com
+
