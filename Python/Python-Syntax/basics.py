5a4277ca6f7835f5dc64c4ab3091e7b55dbeb2e5
Advay Vyas <advay.vyas@gmail.com>
2022-05-13 19:28:43 -0500

    Added other files and added more to the basics file

---
+++ Python/Python-Syntax/basics.py
@@ -0,0 +1,36 @@
+# All if not most examples are from w3schools
+
+# Integer print
+num = 2;
+print(num)
+
+# String print
+string = "hello world"
+print(string)
+
+# Conditional
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Casting
+x = 5
+x = str(x)
+print(x)
+
+# Single or double quotes are the same in Python
+# Dashes, spaces, and numbers are illegal variable names
+
+x, y, z = "Orange", "Banana", "Cherry"
+x = y = z = "Orange"
+
+# Unpacking list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+# To print multiple values, try commas
+x = 5
+y = "John"
+print(x, y)
